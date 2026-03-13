abd8c70a2bba36161c8e96f47ad83f5d0809ca41
MaxiL994 <liebl.maximilian@gmail.com>
2026-03-13 15:28:38 +0100

    fix: SPI back to 40MHz - 80MHz caused display color errors

--- include/display_config.h
+++ include/display_config.h
@@ -16,7 +16,7 @@ public:
             auto cfg = _bus_instance.config();
             cfg.spi_host = SPI2_HOST; // ESP32-S3 FSPI
             cfg.spi_mode = 0;
-            cfg.freq_write = 80000000;
+            cfg.freq_write = 40000000;
             cfg.freq_read  = 16000000;
             cfg.spi_3wire  = true;
             cfg.use_lock   = true;
